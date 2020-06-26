b7ae97ac7456d307564d16ba1ffa47a8da5d621e
David Yue <davidyue5819@gmail.com>
2020-06-26 04:50:48 -0700

    Setup Webpack and basic Node server

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+const app = express();
+
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+
+
+app.listen(PORT, () => {
+    console.log("App started on port " + PORT.toString())
+})
+
+export default app;
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[contenthash].js'
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      template: "public/index.html",
+      filename: 'public/index.html'
+    })
+  ],
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    minimizer: [new UglifyJsPlugin({
+      uglifyOptions: {
+        mangle: true,
+        output: {
+          comments: false
+        }
+      }
+    })],
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+};
+
+module.exports = config;
